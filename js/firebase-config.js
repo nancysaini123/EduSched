011fd29c82f235b7e3e1ebba1b1c76a01963c2fa
Nancy Saini <94524539+nancysaini123@users.noreply.github.com>
2025-06-06 19:51:45 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBkD1uetwt0HO6OHVeKAUNm2jEb1aH_jXo",
   authDomain: "edusched-b0b1a.firebaseapp.com",
   projectId: "edusched-b0b1a",
-  storageBucket: "edusched-b0b1a.appspot.com", // ✅ Fixed this line
+  storageBucket: "edusched-b0b1a.appspot.com",
   messagingSenderId: "120694789732",
   appId: "1:120694789732:web:f66cfc6ba06032099fb171",
   measurementId: "G-YY2CM1LLXL"
